583f47ba099ab8202b47f3db77565476f7396ddb
ieNcos <13810124280@139.com>
2025-10-05 21:47:13 +0800

    flake and other updates

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -3,15 +3,16 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager = {
       # Follow corresponding `release` branch from Home Manager
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-
+  outputs = { nixpkgs, home-manager, ... }: let
+    system = "x86_64-linux" ;
+  in {
     homeConfigurations."ieNcos" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true;};
       modules = [ ./home.nix ];
     };
   };
